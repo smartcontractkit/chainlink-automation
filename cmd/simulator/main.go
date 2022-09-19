a40c0b836136b37975460034e149dfd686201e41
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2022-09-19 09:14:16 -0500

    Small Config Refactor
    The simulator config was refactored to better handle different config
    values and to properly validate the desired configuration before
    starting the simulator.

--- cmd/simulator/main.go
+++ cmd/simulator/main.go
@@ -17,10 +17,10 @@ var (
 	out       = flag.StringP("print-reports", "p", "", "directory to print reports to. prints one report per file")
 	nodes     = flag.IntP("nodes", "s", 3, "number of nodes to simulate")
 	rounds    = flag.IntP("rounds", "d", 2, "OCR rounds to simulate; 0 for no limit")
-	rndTime   = flag.IntP("round-time", "t", 5, "round time in seconds")
-	qTime     = flag.IntP("query-time", "q", 0, "max time to run a Query operation in seconds")
-	oTime     = flag.IntP("observation-time", "o", 0, "max time to run an Observation operation in seconds")
-	rTime     = flag.IntP("report-time", "r", 0, "max time to run a Report operation in seconds")
+	rndTime   = flag.IntP("round-time", "t", 5000, "round time in milliseconds")
+	qTime     = flag.IntP("query-time", "q", 0, "max time to run a Query operation in milliseconds")
+	oTime     = flag.IntP("observation-time", "o", 0, "max time to run an Observation operation in milliseconds")
+	rTime     = flag.IntP("report-time", "r", 0, "max time to run a Report operation in milliseconds")
 	maxRun    = flag.IntP("max-run-time", "m", 0, "max run time in seconds for the simulation")
 	profiler  = flag.Bool("pprof", false, "run pprof server on startup")
 	pprofPort = flag.Int("pprof-port", 6060, "port to serve the profiler on")
@@ -38,7 +38,21 @@ func main() {
 	}
 
 	log.Println("starting simulation")
-	err := runFullSimulation(*contract, *rpc, *out, *nodes, *rounds, *rndTime, *qTime, *oTime, *rTime, *maxRun)
+
+	config := &SimulatorConfig{
+		ContractAddress:      contract,
+		RPC:                  rpc,
+		ReportOutputPath:     out,
+		Nodes:                nodes,
+		Rounds:               rounds,
+		RoundTime:            rndTime,
+		QueryTimeLimit:       qTime,
+		ObservationTimeLimit: oTime,
+		ReportTimeLimit:      rTime,
+		MaxRunTime:           maxRun,
+	}
+
+	err := runFullSimulation(log.Default(), config)
 	if err != nil {
 		panic(err)
 	}
