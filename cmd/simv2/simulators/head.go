62cf41a1c005e478f8e3e27ddd5db95fbbabde49
Roman Behma <begmaroman@gmail.com>
2022-11-28 16:10:02 +0000

    Fixed sim tests

--- cmd/simv2/simulators/head.go
+++ cmd/simv2/simulators/head.go
@@ -11,12 +11,14 @@ import (
 func (ct *SimulatedContract) OnNewHead(ctx context.Context, cb func(header ktypes.BlockKey)) error {
 	initialBlock := int64(0)
 	ticker := time.NewTicker(time.Second * 2)
-	go func() {
-		for range ticker.C {
+	for range ticker.C {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		default:
 			cb(ktypes.BlockKey(big.NewInt(initialBlock).String()))
 			initialBlock++
 		}
-	}()
-
+	}
 	return nil
 }
