db015bd1bfa14ac9fc287753811a3575feaaa910
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2022-12-06 08:59:34 -0500

    Filter out ids until block number
    Once an upkeep id is blocked, continue blocking that upkeep id until
    the block number for the log event has passed.

--- internal/keepers/filter.go
+++ internal/keepers/filter.go
@@ -1,3 +1,18 @@
+/*
+The report coordinator provides 3 main functions:
+Filter
+Accept
+IsTransmissionConfirmed
+
+This has 2 purposes:
+When an id is accepted using the Accept function, the upkeep id should be
+included in the Filter function. This allows an upkeep id to be filtered out
+of a list of upkeep keys.
+
+When an upkeep key is accepted using the Accept function, the upkeep key will
+return false on IsTransmissionConfirmed until a perform log is identified with
+the same key. This allows a coordinated effort on transmit fallbacks.
+*/
 package keepers
 
 import (
@@ -16,15 +31,16 @@ var (
 )
 
 type reportCoordinator struct {
-	logger       *log.Logger
-	registry     types.Registry
-	logs         types.PerformLogProvider
-	minConfs     int
-	idBlocks     *cache[bool] // should clear out when the next perform with this id occurs
-	activeKeys   *cache[bool]
-	cacheCleaner *intervalCacheCleaner[bool]
-	starter      sync.Once
-	chStop       chan struct{}
+	logger         *log.Logger
+	registry       types.Registry
+	logs           types.PerformLogProvider
+	minConfs       int
+	idBlocks       *cache[types.BlockKey]
+	activeKeys     *cache[bool]
+	idCacheCleaner *intervalCacheCleaner[types.BlockKey]
+	cacheCleaner   *intervalCacheCleaner[bool]
+	starter        sync.Once
+	chStop         chan struct{}
 }
 
 func newReportCoordinator(r types.Registry, s time.Duration, cacheClean time.Duration, logs types.PerformLogProvider, minConfs int, logger *log.Logger) *reportCoordinator {
@@ -33,17 +49,18 @@ func newReportCoordinator(r types.Registry, s time.Duration, cacheClean time.Dur
 		registry:   r,
 		logs:       logs,
 		minConfs:   minConfs,
-		idBlocks:   newCache[bool](s),
+		idBlocks:   newCache[types.BlockKey](s),
 		activeKeys: newCache[bool](time.Hour), // 1 hour allows the cleanup routine to clear stale data
 		chStop:     make(chan struct{}),
+		idCacheCleaner: &intervalCacheCleaner[types.BlockKey]{
+			Interval: cacheClean,
+			stop:     make(chan struct{}),
+		},
+		cacheCleaner: &intervalCacheCleaner[bool]{
+			Interval: cacheClean,
+			stop:     make(chan struct{}),
+		},
 	}
-
-	cl := &intervalCacheCleaner[bool]{
-		Interval: cacheClean,
-		stop:     make(chan struct{}),
-	}
-
-	c.cacheCleaner = cl
 
 	runtime.SetFinalizer(c, func(srv *reportCoordinator) { srv.stop() })
 
@@ -53,15 +70,22 @@ func newReportCoordinator(r types.Registry, s time.Duration, cacheClean time.Dur
 }
 
 // Filter returns a filter function that removes upkeep keys that apply to this
-// filter
+// filter. Returns false if a key should be filtered out.
 func (rc *reportCoordinator) Filter() func(types.UpkeepKey) bool {
 	return func(key types.UpkeepKey) bool {
 		id, err := rc.registry.IdentifierFromKey(key)
 		if err != nil {
+			// filter on error
 			return false
 		}
 
-		if _, ok := rc.idBlocks.Get(string(id)); ok {
+		// only apply filter if key id is registered in the cache
+		if bl, ok := rc.idBlocks.Get(string(id)); ok {
+			// only apply filter if key block is after block in cache
+			if len(bl) > 0 && string(key) >= string(bl) {
+				return true
+			}
+
 			return false
 		}
 
@@ -80,7 +104,7 @@ func (rc *reportCoordinator) Accept(key types.UpkeepKey) error {
 		return err
 	}
 
-	rc.idBlocks.Set(string(id), true, defaultExpiration)
+	rc.idBlocks.Set(string(id), types.BlockKey([]byte{}), defaultExpiration)
 	rc.activeKeys.Set(string(key), false, defaultExpiration)
 
 	return nil
@@ -93,17 +117,54 @@ func (rc *reportCoordinator) IsTransmissionConfirmed(key types.UpkeepKey) bool {
 	return !ok || (ok && confirmed)
 }
 
+func (rc *reportCoordinator) checkLogs() {
+	logs, _ := rc.logs.PerformLogs(context.Background())
+
+	// log entries indicate that a perform exists on chain in some
+	// capacity. the existance of an entry means that the transaction
+	// was broadcast by at least one node. reorgs can still happen
+	// causing performs to vanish or get moved to a later block.
+	//
+	// in the case of reorgs causing a perform to be dropped from the
+	// chain, the key is kept in cache even after first detection to
+	// ensure no other nodes attempt to transmit again.
+	for _, l := range logs {
+		if l.Confirmations < int64(rc.minConfs) {
+			continue
+		}
+
+		id, err := rc.registry.IdentifierFromKey(l.Key)
+		if err != nil {
+			continue
+		}
+
+		// Process log if the key hasn't been confirmed yet
+		confirmed, ok := rc.activeKeys.Get(string(l.Key))
+		if ok && !confirmed {
+			// if we detect a log, remove it from the observation filters
+			// to allow it to be reported on again at or after the block in
+			// which it was transmitted
+			rc.idBlocks.Set(string(id), l.TransmitBlock, defaultExpiration)
+
+			// set state of key to indicate that the report was transmitted
+			// setting a key in this way also blocks it in Accept even if
+			// Accept was never called for on a single node for this key
+			rc.activeKeys.Set(string(l.Key), true, defaultExpiration)
+		}
+	}
+}
+
 func (rc *reportCoordinator) start() {
 	rc.starter.Do(func() {
 		go rc.run()
-		go rc.cacheCleaner.Run(rc.idBlocks)
+		go rc.idCacheCleaner.Run(rc.idBlocks)
 		go rc.cacheCleaner.Run(rc.activeKeys)
 	})
 }
 
 func (rc *reportCoordinator) stop() {
 	rc.chStop <- struct{}{}
-	rc.cacheCleaner.stop <- struct{}{}
+	rc.idCacheCleaner.stop <- struct{}{}
 	rc.cacheCleaner.stop <- struct{}{}
 }
 
@@ -117,39 +178,7 @@ func (rc *reportCoordinator) run() {
 		select {
 		case <-timer.C:
 			startTime := time.Now()
-			logs, _ := rc.logs.PerformLogs(context.Background())
-
-			// log entries indicate that a perform exists on chain in some
-			// capacity. the existance of an entry means that the transaction
-			// was broadcast by at least one node. reorgs can still happen
-			// causing performs to vanish or get moved to a later block.
-			//
-			// in the case of reorgs causing a perform to be dropped from the
-			// chain, the key is kept in cache even after first detection to
-			// ensure no other nodes attempt to transmit again.
-			for _, log := range logs {
-				if log.Confirmations < int64(rc.minConfs) {
-					continue
-				}
-
-				id, err := rc.registry.IdentifierFromKey(log.Key)
-				if err != nil {
-					continue
-				}
-
-				// Process log if the key hasn't been confirmed yet
-				confirmed, ok := rc.activeKeys.Get(string(log.Key))
-				if ok && !confirmed {
-					// if we detect a log, remove it from the observation filters
-					// to allow it to be reported on again
-					rc.idBlocks.Delete(string(id))
-
-					// set state of key to indicate that the report was transmitted
-					// setting a key in this way also blocks it in Accept even if
-					// Accept was never called for on a single node for this key
-					rc.activeKeys.Set(string(log.Key), true, defaultExpiration)
-				}
-			}
+			rc.checkLogs()
 
 			// attempt to ahere to a cadence of at least every second
 			// a slow DB will cause the cadence to increase. these cases are logged
--- internal/keepers/filter_test.go
+++ internal/keepers/filter_test.go
@@ -12,7 +12,7 @@ import (
 	"github.com/smartcontractkit/ocr2keepers/pkg/types"
 )
 
-func TestNewFilter(t *testing.T) {
+func TestReportCoordinator(t *testing.T) {
 	mr := types.NewMockRegistry(t)
 	mp := types.NewMockPerformLogProvider(t)
 
@@ -22,54 +22,96 @@ func TestNewFilter(t *testing.T) {
 		logger:     l,
 		registry:   mr,
 		logs:       mp,
-		idBlocks:   newCache[bool](time.Second),
+		idBlocks:   newCache[types.BlockKey](time.Second),
 		activeKeys: newCache[bool](time.Minute),
+		minConfs:   1,
 		chStop:     make(chan struct{}),
 	}
 
 	// set up the mocks and mock data
-	key := types.UpkeepKey("1|1")
-	key2 := types.UpkeepKey("2|1")
-	id := types.UpkeepIdentifier("1")
+	key1Block1 := types.UpkeepKey("1|1")
+	key1Block2 := types.UpkeepKey("2|1")
+	id1 := types.UpkeepIdentifier("1")
+	bk2 := types.BlockKey("2")
+	filter := rc.Filter()
 
-	// calling filter at this point should return true because the key has not
-	// yet been added to the filter
-	f := rc.Filter()
-	mr.Mock.On("IdentifierFromKey", key).Return(id, nil)
-	assert.Equal(t, true, f(key), "filter should not filter out key becase key has not been set")
+	t.Run("FilterBeforeAccept", func(t *testing.T) {
+		// calling filter at this point should return true because the key has not
+		// yet been added to the filter
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		assert.Equal(t, true, filter(key1Block1), "should not filter out key 1 at block 1: key has not been accepted")
 
-	// is transmission confirmed should also return true because the key has
-	// not been set in the filter
-	assert.Equal(t, true, rc.IsTransmissionConfirmed(key), "transmit should be confirmed because key has not been set")
+		mr.Mock.On("IdentifierFromKey", key1Block2).Return(id1, nil)
+		assert.Equal(t, true, filter(key1Block2), "should not filter out key 1 at block 2: key has not been accepted")
 
-	mr.Mock.On("IdentifierFromKey", key).Return(id, nil)
-	assert.NoError(t, rc.Accept(key), "no error expected from setting the key")
-	assert.ErrorIs(t, rc.Accept(key), ErrKeyAlreadySet, "key should not be accepted again and should return an error")
+		// is transmission confirmed should also return true because the key has
+		// not been set in the filter
+		// this would block any transmissions for this upkeep key (block & id)
+		assert.Equal(t, true, rc.IsTransmissionConfirmed(key1Block1), "transmit will return confirmed: key has not been accepted")
+	})
 
-	mr.Mock.On("IdentifierFromKey", key).Return(id, nil)
-	assert.Equal(t, false, f(key), "key should be included in filter")
-	assert.Equal(t, false, rc.IsTransmissionConfirmed(key), "transmit should not be confirmed because the key is now set, but no logs have been identified")
+	t.Run("Accept", func(t *testing.T) {
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		assert.NoError(t, rc.Accept(key1Block1), "no error expected from accepting the key")
+		assert.ErrorIs(t, rc.Accept(key1Block1), ErrKeyAlreadySet, "key should not be accepted again and should return an error")
+	})
 
-	mr.Mock.On("IdentifierFromKey", key).Return(id, nil)
-	mr.Mock.On("IdentifierFromKey", key2).Return(id, nil)
-	mp.Mock.On("PerformLogs", mock.Anything).Return([]types.PerformLog{
-		{Key: key},
-		{Key: key2},
-	}, nil)
+	t.Run("FilterAfterAccept", func(t *testing.T) {
+		// no logs have been read at this point so the upkeep key should be filtered
+		// out at all block numbers
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		assert.Equal(t, false, filter(key1Block1), "filter should return false to indicate key should be filtered out at block 1")
 
-	// simulate starting the run process
-	go rc.run()
+		mr.Mock.On("IdentifierFromKey", key1Block2).Return(id1, nil)
+		assert.Equal(t, false, filter(key1Block2), "filter should return false to indicate key should be filtered out at block 2")
 
-	// wait for only 1 cycle to complete
-	<-time.After(1100 * time.Millisecond)
+		assert.Equal(t, false, rc.IsTransmissionConfirmed(key1Block1), "transmit should not be confirmed: key is now set, but no logs have been identified")
 
-	// stop the run process
-	rc.chStop <- struct{}{}
+		// returning true for an unset key prevents any node from transmitting a key that was never accepted
+		assert.Equal(t, true, rc.IsTransmissionConfirmed(key1Block2), "transmit should be confirmed: key was not set for block 2")
+	})
 
-	assert.Equal(t, true, rc.IsTransmissionConfirmed(key), "transmit should be confirmed after logs are read for the key")
+	t.Run("CollectLogsWithMinConfirmations_LessThan", func(t *testing.T) {
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		mp.Mock.On("PerformLogs", mock.Anything).Return([]types.PerformLog{
+			{Key: key1Block1, TransmitBlock: bk2, Confirmations: 0},
+		}, nil).Once()
 
-	assert.ErrorIs(t, rc.Accept(key), ErrKeyAlreadySet, "key should not be accepted after transmit confirmed and should return an error")
-	assert.NoError(t, rc.Accept(key2), "key2 should be able to accept since log should be ignored if it was not accepted before")
+		rc.checkLogs()
+
+		// perform log didn't have the threshold number of confirmations
+		// making the key still locked at all blocks
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		assert.Equal(t, false, filter(key1Block1), "filter should return false to indicate key should be filtered out at block 1")
+
+		mr.Mock.On("IdentifierFromKey", key1Block2).Return(id1, nil)
+		assert.Equal(t, false, filter(key1Block2), "filter should return false to indicate key should be filtered out at block 2")
+
+		assert.Equal(t, false, rc.IsTransmissionConfirmed(key1Block1), "transmit should not be confirmed: the key is now set, but no logs have been identified")
+		assert.Equal(t, true, rc.IsTransmissionConfirmed(key1Block2), "transmit should be confirmed: key was not set for block 2")
+	})
+
+	t.Run("CollectLogsWithMinConfirmations_GreaterThan", func(t *testing.T) {
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		mp.Mock.On("PerformLogs", mock.Anything).Return([]types.PerformLog{
+			{Key: key1Block1, TransmitBlock: bk2, Confirmations: 1},
+		}, nil).Once()
+
+		rc.checkLogs()
+
+		// because the transmit block is block 2, the filter should continue
+		// to filter out key up to block 2
+		mr.Mock.On("IdentifierFromKey", key1Block1).Return(id1, nil)
+		assert.Equal(t, false, filter(key1Block1), "filter should return false to indicate key should be filtered out at block 1")
+
+		mr.Mock.On("IdentifierFromKey", key1Block2).Return(id1, nil)
+		assert.Equal(t, true, filter(key1Block2), "filter should return true to indicate key should not be filtered out at block 2")
+
+		assert.Equal(t, true, rc.IsTransmissionConfirmed(key1Block1), "transmit should be confirmed")
+		assert.Equal(t, true, rc.IsTransmissionConfirmed(key1Block2), "transmit should be confirmed: key was not set for block 2")
+
+		assert.ErrorIs(t, rc.Accept(key1Block1), ErrKeyAlreadySet, "key should not be accepted after transmit confirmed and should return an error")
+	})
 
 	mp.AssertExpectations(t)
 	mr.AssertExpectations(t)
