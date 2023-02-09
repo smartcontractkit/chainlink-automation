b9f3d0960da1bfdf98a96fc58b57a11e46291257
Roman Behma <13855864+begmaroman@users.noreply.github.com>
2023-02-09 17:12:47 +0100

    Updated dedupe function to use fmt.Stringer (#66)

--- internal/keepers/util_test.go
+++ internal/keepers/util_test.go
@@ -13,6 +13,12 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+type testStringer string
+
+func (ts testStringer) String() string {
+	return string(ts)
+}
+
 func TestCryptoShuffler(t *testing.T) {
 	expectected := []int{1, 2, 3, 4, 5}
 	test := make([]int, len(expectected))
@@ -30,34 +36,35 @@ func TestCryptoShuffler(t *testing.T) {
 func TestDedupe(t *testing.T) {
 	tests := []struct {
 		Name           string
-		Sets           [][]string
-		ExpectedResult []string
+		Sets           [][]testStringer
+		ExpectedResult []testStringer
 		ExpectedError  error
 	}{
 		{Name: "Empty Matching Sets", ExpectedResult: nil, ExpectedError: ErrNotEnoughInputs},
-		{Name: "Single Matching Set", Sets: [][]string{{"1", "2", "3"}}, ExpectedResult: []string{"1", "2", "3"}},
-		{Name: "Double Identical", Sets: [][]string{{"1", "2", "3"}, {"1", "2", "3"}}, ExpectedResult: []string{"1", "2", "3"}},
-		{Name: "Double No Match", Sets: [][]string{{"1", "2", "3"}, {"4", "5", "6"}}, ExpectedResult: []string{"1", "2", "3", "4", "5", "6"}},
-		{Name: "Triple Identical", Sets: [][]string{{"1", "2", "3"}, {"1", "2", "3"}, {"1", "2", "3"}}, ExpectedResult: []string{"1", "2", "3"}},
-		{Name: "Triple No Match", Sets: [][]string{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}, ExpectedResult: []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"}},
-		{Name: "Double w/ Single Overlap", Sets: [][]string{{"1", "2", "3"}, {"3", "4", "5"}}, ExpectedResult: []string{"1", "2", "3", "4", "5"}},
-		{Name: "Double w/ Single Overlap and Gap", Sets: [][]string{{"1", "2", "3", "6", "7"}, {"3", "4", "5"}}, ExpectedResult: []string{"1", "2", "3", "4", "5", "6", "7"}},
-		{Name: "Double w/ Double Overlap and Gap", Sets: [][]string{{"1", "2", "3", "4", "6", "7"}, {"3", "4", "5"}}, ExpectedResult: []string{"1", "2", "3", "4", "5", "6", "7"}},
-		{Name: "Triple w/ Single Overlap", Sets: [][]string{{"1", "2", "3"}, {"1", "2", "3"}, {"3", "4", "5"}}, ExpectedResult: []string{"1", "2", "3", "4", "5"}},
-		{Name: "Triple w/ Single Overlap and Gap", Sets: [][]string{{"1", "2", "4"}, {"1", "2", "3", "4"}, {"3", "4", "5"}}, ExpectedResult: []string{"1", "2", "3", "4", "5"}},
+		{Name: "Single Matching Set", Sets: [][]testStringer{{"1", "2", "3"}}, ExpectedResult: []testStringer{"1", "2", "3"}},
+		{Name: "Double Identical", Sets: [][]testStringer{{"1", "2", "3"}, {"1", "2", "3"}}, ExpectedResult: []testStringer{"1", "2", "3"}},
+		{Name: "Double No Match", Sets: [][]testStringer{{"1", "2", "3"}, {"4", "5", "6"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5", "6"}},
+		{Name: "Triple Identical", Sets: [][]testStringer{{"1", "2", "3"}, {"1", "2", "3"}, {"1", "2", "3"}}, ExpectedResult: []testStringer{"1", "2", "3"}},
+		{Name: "Triple No Match", Sets: [][]testStringer{{"1", "2", "3"}, {"4", "5", "6"}, {"7", "8", "9"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5", "6", "7", "8", "9"}},
+		{Name: "Double w/ Single Overlap", Sets: [][]testStringer{{"1", "2", "3"}, {"3", "4", "5"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5"}},
+		{Name: "Double w/ Single Overlap and Gap", Sets: [][]testStringer{{"1", "2", "3", "6", "7"}, {"3", "4", "5"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5", "6", "7"}},
+		{Name: "Double w/ Double Overlap and Gap", Sets: [][]testStringer{{"1", "2", "3", "4", "6", "7"}, {"3", "4", "5"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5", "6", "7"}},
+		{Name: "Triple w/ Single Overlap", Sets: [][]testStringer{{"1", "2", "3"}, {"1", "2", "3"}, {"3", "4", "5"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5"}},
+		{Name: "Triple w/ Single Overlap and Gap", Sets: [][]testStringer{{"1", "2", "4"}, {"1", "2", "3", "4"}, {"3", "4", "5"}}, ExpectedResult: []testStringer{"1", "2", "3", "4", "5"}},
 	}
 
 	for _, test := range tests {
 		t.Run(test.Name, func(t *testing.T) {
-			var matches []string
+			var matches []testStringer
 			var err error
 
 			matches, err = dedupe(test.Sets)
-
 			if test.ExpectedError != nil {
 				assert.ErrorIs(t, err, test.ExpectedError)
 			} else {
-				sort.Strings(matches)
+				sort.Slice(matches, func(i, j int) bool {
+					return matches[i] < matches[j]
+				})
 				assert.Equal(t, test.ExpectedResult, matches)
 			}
 		})
