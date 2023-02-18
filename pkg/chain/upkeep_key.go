0474d444ac7013928f074453c5ad5f3e13c0bf66
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-02-18 14:59:34 +0000

    add TODOs

--- pkg/chain/upkeep_key.go
+++ pkg/chain/upkeep_key.go
@@ -9,6 +9,7 @@ import (
 	"github.com/smartcontractkit/ocr2keepers/pkg/types"
 )
 
+// TODO (AUTO-2014), find a better place for these concrete types than chain package
 type UpkeepKey []byte
 
 // NewUpkeepKey is the constructor of UpkeepKey
--- pkg/chain/upkeep_observation.go
+++ pkg/chain/upkeep_observation.go
@@ -4,6 +4,7 @@ import (
 	"github.com/smartcontractkit/ocr2keepers/pkg/types"
 )
 
+// TODO (AUTO-2014), find a better place for these concrete types than chain package
 type UpkeepObservation struct {
 	BlockKey          BlockKey                 `json:"1"`
 	UpkeepIdentifiers []types.UpkeepIdentifier `json:"2"`
