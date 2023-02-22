634fb2a8a4f7d80d2350ee9c42383354035cb4b8
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-02-22 14:12:46 +0000

    regen type

--- pkg/types/perform_log_provider.generated.go
+++ pkg/types/perform_log_provider.generated.go
@@ -37,6 +37,29 @@ func (_m *MockPerformLogProvider) PerformLogs(_a0 context.Context) ([]PerformLog
 	return r0, r1
 }
 
+// ReorgLogs provides a mock function with given fields: _a0
+func (_m *MockPerformLogProvider) ReorgLogs(_a0 context.Context) ([]ReorgLog, error) {
+	ret := _m.Called(_a0)
+
+	var r0 []ReorgLog
+	if rf, ok := ret.Get(0).(func(context.Context) []ReorgLog); ok {
+		r0 = rf(_a0)
+	} else {
+		if ret.Get(0) != nil {
+			r0 = ret.Get(0).([]ReorgLog)
+		}
+	}
+
+	var r1 error
+	if rf, ok := ret.Get(1).(func(context.Context) error); ok {
+		r1 = rf(_a0)
+	} else {
+		r1 = ret.Error(1)
+	}
+
+	return r0, r1
+}
+
 // NewMockPerformLogProvider creates a new instance of MockPerformLogProvider. It also registers the testing.TB interface on the mock and a cleanup function to assert the mocks expectations.
 func NewMockPerformLogProvider(t testing.TB) *MockPerformLogProvider {
 	mock := &MockPerformLogProvider{}
