fcf9e2dbf2bca42a4f1f3fcd1d674d696b037114
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2023-02-22 18:06:36 -0500

    initial test cases

---
+++ internal/malicious/parse.go
@@ -0,0 +1,65 @@
+package malicious
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/smartcontractkit/ocr2keepers/pkg/chain"
+	"github.com/smartcontractkit/ocr2keepers/pkg/types"
+)
+
+// ObservationParseError produces an encoded json object that does not properly
+// parse into a valid observation.
+func ObservationParseError(_ context.Context, _ []byte, _ error) (string, []byte, error) {
+	type internal struct {
+		D string
+		E uint
+	}
+
+	type external struct {
+		A int
+		B string
+		C internal
+	}
+
+	b, err := json.Marshal(external{5, "string", internal{"", 8}})
+	return "Invalid Observation Structure", b, err
+}
+
+// NilBytesObservation returns a nil bytes observation
+func NilBytesObservation(_ context.Context, _ []byte, _ error) (string, []byte, error) {
+	return "Nil Bytes Observation", nil, nil
+}
+
+// EmptyBytesObservation returns a nil bytes observation
+func EmptyBytesObservation(_ context.Context, _ []byte, _ error) (string, []byte, error) {
+	return "Empty Bytes Observation", nil, nil
+}
+
+type UpkeepQuerant func(context.Context) (types.BlockKey, types.UpkeepResults, error)
+
+// ObservationExtraFields produces an encoded json object that does not properly
+// parse into a valid observation.
+func ObservationExtraFields(ctx context.Context, original []byte, _ error) (string, []byte, error) {
+	name := "Observation Extra Fields"
+
+	type bulkyObservation struct {
+		BlockKey          chain.BlockKey           `json:"1"`
+		UpkeepIdentifiers []types.UpkeepIdentifier `json:"2"`
+		OtherData         []int                    `json:"3"`
+	}
+
+	var ob chain.UpkeepObservation
+	if err := json.Unmarshal(original, &ob); err != nil {
+		return name, nil, err
+	}
+
+	observation := bulkyObservation{
+		BlockKey:          ob.BlockKey,
+		UpkeepIdentifiers: ob.UpkeepIdentifiers,
+		OtherData:         []int{1, 2, 3, 4, 5, 6, 7},
+	}
+
+	b, err := json.Marshal(observation)
+	return name, b, err
+}
