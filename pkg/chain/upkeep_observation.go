70d6ea08084cbcab84eceb4d355421ead45de1fe
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-02-23 15:55:37 +0000

    Improve validation against negative keys

--- pkg/chain/upkeep_observation.go
+++ pkg/chain/upkeep_observation.go
@@ -44,6 +44,9 @@ func (u *UpkeepObservation) validate(uo upkeepObservation) error {
 	if bl.String() != uo.BlockKey.String() {
 		return ErrInvalidBlockKey
 	}
+	if bl.Cmp(big.NewInt(0)) == -1 {
+		return ErrInvalidBlockKey
+	}
 
 	for _, ui := range uo.UpkeepIdentifiers {
 		uiInt, ok := ui.BigInt()
@@ -51,6 +54,9 @@ func (u *UpkeepObservation) validate(uo upkeepObservation) error {
 			return ErrUpkeepKeyNotParsable
 		}
 		if uiInt.String() != string(ui) {
+			return ErrInvalidUpkeepIdentifier
+		}
+		if uiInt.Cmp(big.NewInt(0)) == -1 {
 			return ErrInvalidUpkeepIdentifier
 		}
 	}
