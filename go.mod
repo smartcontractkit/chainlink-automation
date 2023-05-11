c5d9537542fdc8a46a52af2782094ad18eba2865
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2023-05-11 12:27:45 -0500

    Chore/flatten project files to pkg (#109)
    * Flatten OCR Keepers to /pkg
    
    The goal is to move all types that could be used externally into
    the /pkg package. This helps simplify the plugin surface area as
    complex code isolation isn't really needed at this point.
    
    * Separate Marshalling and Validation
    
    Because the validator type was introduced and is an interface instead
    of a concrete list of validations, the marshalling behavior and
    validation needed to be separated. Likewise, the tests were separated.
    
    To complete full validation testing, the individual validators should
    be tested separately.
    
    * upgrade tool version
    
    * remove sim-v1 cmd in makefile
    
    * fix linting errors

--- go.mod
+++ go.mod
@@ -1,6 +1,6 @@
 module github.com/smartcontractkit/ocr2keepers
 
-go 1.19
+go 1.20
 
 replace (
 	github.com/btcsuite/btcd => github.com/btcsuite/btcd v0.23.3
---
+++ pkg/encode_test.go
@@ -0,0 +1,52 @@
+package ocr2keepers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/smartcontractkit/ocr2keepers/pkg/chain"
+	ktypes "github.com/smartcontractkit/ocr2keepers/pkg/types"
+)
+
+func BenchmarkDecode(b *testing.B) {
+	key1 := chain.UpkeepKey([]byte("1239487928374|18768923479234987"))
+	key2 := chain.UpkeepKey([]byte("1239487928374|18768923479234989"))
+	key3 := chain.UpkeepKey([]byte("1239487928375|18768923479234987"))
+
+	encoded := mustEncodeKeys([]ktypes.UpkeepKey{key1, key2, key3})
+
+	b.ResetTimer()
+	for n := 0; n < b.N; n++ {
+		var keys []ktypes.UpkeepKey
+
+		b.StartTimer()
+		err := decode(encoded, &keys)
+		b.StopTimer()
+
+		if err != nil {
+			b.FailNow()
+		}
+	}
+}
+
+func Test_encode(t *testing.T) {
+	t.Run("successfully encodes a string", func(t *testing.T) {
+		b, err := encode([]string{"1", "2", "3"})
+		assert.Nil(t, err)
+		// the encoder appends a new line to the output string
+		assert.Equal(t, b, []byte(`["1","2","3"]
+`))
+	})
+
+	t.Run("fails to encode a channel", func(t *testing.T) {
+		b, err := encode(make(chan int))
+		assert.NotNil(t, err)
+		assert.Nil(t, b)
+	})
+}
+
+func mustEncodeKeys(keys []ktypes.UpkeepKey) []byte {
+	b, _ := encode(keys)
+	return b
+}
