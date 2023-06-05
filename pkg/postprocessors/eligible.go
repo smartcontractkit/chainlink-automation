419be6091dcb1bf803658483cd7155378550b496
Fergal Gribben <fergal.gribben@smartcontract.com>
2023-06-05 22:32:19 +0100

    Add comments

--- pkg/postprocessors/eligible.go
+++ pkg/postprocessors/eligible.go
@@ -6,11 +6,16 @@ import (
 	ocr2keepers "github.com/smartcontractkit/ocr2keepers/pkg"
 )
 
+// checkResultAdder is a general interface for a result store that accepts check results
 type checkResultAdder interface {
+	// Add inserts the provided check result in the store
 	Add(ocr2keepers.CheckResult)
 }
 
+// PostProcessor is the general interface for a processing function after checking eligibility
+// status
 type PostProcessor interface {
+	// PostProcess takes a slice of results where eligibility status is known
 	PostProcess(context.Context, []ocr2keepers.CheckResult) error
 }
 
