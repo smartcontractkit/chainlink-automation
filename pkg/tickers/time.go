6e0876261df56656ffa402e52cc36a41903cd743
ferglor <19188060+ferglor@users.noreply.github.com>
2023-06-13 05:43:10 +0100

    Add a basic time ticker (#127)
    * Add a basic time ticker
    
    * Rework implementation to return a Tick from the getter function
    
    * Clean up duplicate UpkeepPayload
    
    * Derive the interval context from a parent context created in the constructor
    
    * Clean up ticker run

---
+++ pkg/tickers/time.go
@@ -0,0 +1,77 @@
+package time
+
+import (
+	"context"
+	"log"
+	"time"
+
+	ocr2keepers "github.com/smartcontractkit/ocr2keepers/pkg"
+)
+
+var (
+	logPrintf = log.Printf
+)
+
+type observer interface {
+	Process(context.Context, Tick) error
+}
+
+// Ticker is a process that runs interval ticks.
+type Ticker interface {
+	Start() error
+	Stop() error
+}
+
+// Tick is the container for the individual tick
+type Tick interface {
+	// GetUpkeeps provides upkeeps scoped to the tick
+	GetUpkeeps(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error)
+}
+
+type getUpkeepsFn func(context.Context, time.Time) (Tick, error)
+
+type timeTicker struct {
+	interval time.Duration
+	ticker   *time.Ticker
+	observer observer
+	getterFn getUpkeepsFn
+	ctx      context.Context
+	cancelFn context.CancelFunc
+}
+
+func NewTimeTicker(interval time.Duration, observer observer, getterFn getUpkeepsFn) *timeTicker {
+	ctx, cancelFn := context.WithCancel(context.Background())
+	t := &timeTicker{
+		interval: interval,
+		ticker:   time.NewTicker(interval),
+		observer: observer,
+		getterFn: getterFn,
+		ctx:      ctx,
+		cancelFn: cancelFn,
+	}
+	go t.Start()
+	return t
+}
+
+func (t *timeTicker) Start() {
+	for tm := range t.ticker.C {
+		func() {
+			ctx, cancelFn := context.WithTimeout(t.ctx, t.interval)
+			defer cancelFn()
+
+			tick, err := t.getterFn(ctx, tm)
+			if err != nil {
+				logPrintf("error fetching tick: %s", err.Error())
+			}
+
+			if err := t.observer.Process(ctx, tick); err != nil {
+				logPrintf("error processing observer: %s", err.Error())
+			}
+		}()
+	}
+}
+
+func (t *timeTicker) Stop() {
+	t.cancelFn()
+	t.ticker.Stop()
+}
---
+++ pkg/tickers/time_test.go
@@ -0,0 +1,210 @@
+package time
+
+import (
+	"context"
+	"fmt"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/pkg/errors"
+	"github.com/stretchr/testify/assert"
+
+	ocr2keepers "github.com/smartcontractkit/ocr2keepers/pkg"
+)
+
+type mockObserver struct {
+	processFn func(context.Context, Tick) error
+}
+
+func (o *mockObserver) Process(ctx context.Context, t Tick) error {
+	return o.processFn(ctx, t)
+}
+
+type mockUpkeepPayload struct {
+	data string
+}
+
+type mockTick struct {
+	getUpkeepsFn func(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error)
+}
+
+func (t *mockTick) GetUpkeeps(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error) {
+	return t.getUpkeepsFn(ctx)
+}
+
+func TestNewTimeTicker(t *testing.T) {
+	t.Run("creates new time ticker with a counting observer", func(t *testing.T) {
+		callCount := 0
+		observr := &mockObserver{
+			processFn: func(ctx context.Context, t Tick) error {
+				callCount++
+				return nil
+			},
+		}
+		upkeepsFn := func(ctx context.Context, t time.Time) (Tick, error) {
+			return &mockTick{
+				getUpkeepsFn: func(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error) {
+					return nil, nil
+				},
+			}, nil
+		}
+
+		ticker := NewTimeTicker(100*time.Millisecond, observr, upkeepsFn)
+
+		time.Sleep(450 * time.Millisecond)
+
+		ticker.Stop()
+
+		assert.Equal(t, callCount, 4)
+		time.Sleep(200 * time.Millisecond)
+		assert.Equal(t, callCount, 4)
+	})
+
+	t.Run("creates new time ticker with a processing observer", func(t *testing.T) {
+		callCount := 0
+
+		upkeepPayloads := []ocr2keepers.UpkeepPayload{
+			&mockUpkeepPayload{
+				data: "first mock data payload",
+			},
+			&mockUpkeepPayload{
+				data: "second mock data payload",
+			},
+		}
+
+		observr := &mockObserver{
+			processFn: func(ctx context.Context, tick Tick) error {
+				callCount++
+
+				upkeeps, err := tick.GetUpkeeps(ctx)
+				if err != nil {
+					return err
+				}
+
+				if !reflect.DeepEqual(upkeeps, upkeepPayloads) {
+					t.Fatal("unexpected payloads")
+				}
+				return nil
+			},
+		}
+		upkeepsFn := func(ctx context.Context, t time.Time) (Tick, error) {
+			return &mockTick{
+				getUpkeepsFn: func(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error) {
+					return upkeepPayloads, nil
+				},
+			}, nil
+		}
+
+		ticker := NewTimeTicker(100*time.Millisecond, observr, upkeepsFn)
+
+		time.Sleep(450 * time.Millisecond)
+
+		ticker.Stop()
+
+		assert.Equal(t, callCount, 4)
+	})
+
+	t.Run("creates a ticker with an observer that errors when the getter errors", func(t *testing.T) {
+		var msg string
+		oldLogPrintf := logPrintf
+		logPrintf = func(format string, v ...any) {
+			msg = fmt.Sprintf(format, v...)
+		}
+		defer func() {
+			logPrintf = oldLogPrintf
+		}()
+
+		observr := &mockObserver{
+			processFn: func(ctx context.Context, tick Tick) error {
+				return errors.New("boom")
+			},
+		}
+		upkeepsFn := func(ctx context.Context, t time.Time) (Tick, error) {
+			return nil, errors.New("error fetching tick")
+		}
+
+		ticker := NewTimeTicker(100*time.Millisecond, observr, upkeepsFn)
+
+		time.Sleep(450 * time.Millisecond)
+
+		ticker.Stop()
+
+		assert.Equal(t, msg, "error processing observer: boom")
+	})
+
+	t.Run("creates a ticker with an observer that errors on processing", func(t *testing.T) {
+		var msg string
+		oldLogPrintf := logPrintf
+		logPrintf = func(format string, v ...any) {
+			msg = fmt.Sprintf(format, v...)
+		}
+		defer func() {
+			logPrintf = oldLogPrintf
+		}()
+
+		observr := &mockObserver{
+			processFn: func(ctx context.Context, tick Tick) error {
+				return errors.New("process error")
+			},
+		}
+		upkeepsFn := func(ctx context.Context, t time.Time) (Tick, error) {
+			return &mockTick{
+				getUpkeepsFn: func(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error) {
+					return nil, nil
+				},
+			}, nil
+		}
+
+		ticker := NewTimeTicker(100*time.Millisecond, observr, upkeepsFn)
+
+		time.Sleep(450 * time.Millisecond)
+
+		ticker.Stop()
+
+		assert.Equal(t, msg, "error processing observer: process error")
+	})
+
+	t.Run("creates a ticker with an observer that exceeds the processing timeout", func(t *testing.T) {
+		successfulCallCount := 0
+
+		var msg string
+		oldLogPrintf := logPrintf
+		logPrintf = func(format string, v ...any) {
+			msg = fmt.Sprintf(format, v...)
+		}
+		defer func() {
+			logPrintf = oldLogPrintf
+		}()
+
+		firstRun := true
+
+		observr := &mockObserver{
+			processFn: func(ctx context.Context, tick Tick) error {
+				if firstRun {
+					firstRun = false
+					<-ctx.Done()
+					return ctx.Err()
+				}
+				successfulCallCount++
+				return nil
+			},
+		}
+		upkeepsFn := func(ctx context.Context, t time.Time) (Tick, error) {
+			return &mockTick{
+				getUpkeepsFn: func(ctx context.Context) ([]ocr2keepers.UpkeepPayload, error) {
+					return nil, nil
+				},
+			}, nil
+		}
+
+		ticker := NewTimeTicker(100*time.Millisecond, observr, upkeepsFn)
+
+		time.Sleep(450 * time.Millisecond)
+
+		ticker.Stop()
+
+		assert.Equal(t, msg, "error processing observer: context deadline exceeded")
+		assert.Equal(t, successfulCallCount, 3)
+	})
+}
