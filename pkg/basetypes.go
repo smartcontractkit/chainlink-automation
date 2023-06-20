319923ebdd45bb10536452c5a433e583c259f2b8
Amir Y <83904651+amirylm@users.noreply.github.com>
2023-06-20 18:52:25 +0300

    aligned base types (#154)

--- pkg/basetypes.go
+++ pkg/basetypes.go
@@ -1,6 +1,10 @@
 package ocr2keepers
 
 import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"math/big"
 	"strings"
 )
 
@@ -57,6 +61,47 @@ type UpkeepPayload struct {
 	Upkeep ConfiguredUpkeep
 	// CheckData is the data used to check the upkeep
 	CheckData []byte
-	// Tick is the event that triggered the upkeep to be checked
-	Tick interface{}
+	// Trigger is the event that triggered the upkeep to be checked
+	Trigger Trigger
+}
+
+func NewUpkeepPayload(uid *big.Int, tp int, trigger Trigger, checkData []byte) UpkeepPayload {
+	p := UpkeepPayload{
+		Upkeep: ConfiguredUpkeep{
+			ID:   UpkeepIdentifier(uid.Bytes()),
+			Type: tp,
+		},
+		Trigger:   trigger,
+		CheckData: checkData,
+	}
+	p.ID = p.GenerateID()
+	return p
+}
+
+func (p UpkeepPayload) GenerateID() string {
+	id := fmt.Sprintf("%s:%s", p.Upkeep.ID, p.Trigger)
+	idh := md5.Sum([]byte(id))
+	return hex.EncodeToString(idh[:])
+}
+
+type Trigger struct {
+	// BlockNumber is the block number of the corresponding block
+	BlockNumber int64
+	// BlockHash is the block hash of the corresponding block
+	BlockHash string
+	// Extension is the extensions data that can differ between triggers.
+	// e.g. for tx hash and log id for log triggers.
+	Extension interface{}
+}
+
+func NewTrigger(blockNumber int64, blockHash string, extension interface{}) Trigger {
+	return Trigger{
+		BlockNumber: blockNumber,
+		BlockHash:   blockHash,
+		Extension:   extension,
+	}
+}
+
+func (t Trigger) String() string {
+	return fmt.Sprintf("%d:%s:%+v", t.BlockNumber, t.BlockHash, t.Extension)
 }
