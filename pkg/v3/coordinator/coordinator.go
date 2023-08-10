13b87116f58b4dd3b2189783f462e50caac57617
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-08-10 22:28:12 +0100

    add comments in coordinator

--- pkg/v3/coordinator/coordinator.go
+++ pkg/v3/coordinator/coordinator.go
@@ -50,33 +50,34 @@ func NewCoordinator(transmitEventProvider ocr2keepers.TransmitEventProvider, upk
 func (c *coordinator) ShouldAccept(reportedUpkeep ocr2keepers.ReportedUpkeep) bool {
 	if v, ok := c.cache.Get(reportedUpkeep.WorkID); !ok {
 		c.cache.Set(reportedUpkeep.WorkID, record{
-			checkBlockNumber: reportedUpkeep.Trigger.BlockNumber,
+			checkBlockNumber:      reportedUpkeep.Trigger.BlockNumber,
+			isTransmissionPending: true,
 		}, util.DefaultCacheExpiration)
 		return true
 	} else if v.checkBlockNumber < reportedUpkeep.Trigger.BlockNumber {
 		c.cache.Set(reportedUpkeep.WorkID, record{
 			checkBlockNumber:      reportedUpkeep.Trigger.BlockNumber,
-			isTransmissionPending: v.isTransmissionPending,
-			transmitType:          v.transmitType,
-			transmitBlockNumber:   v.transmitBlockNumber,
+			isTransmissionPending: true,
 		}, util.DefaultCacheExpiration)
 		return true
-	} else if v.checkBlockNumber > reportedUpkeep.Trigger.BlockNumber {
-		return false
 	}
-	// TODO should we not accept if the reported payload check block is equal to the cached check block?
+	// We are already waiting on a higher checkBlockNumber so no need to accept this report
 	return false
 }
 
 func (c *coordinator) ShouldTransmit(reportedUpkeep ocr2keepers.ReportedUpkeep) bool {
 	if v, ok := c.cache.Get(reportedUpkeep.WorkID); !ok {
+		// We never saw this report, so don't try to transmit
+		// Can happen in edge cases when plugin restarts after shouldAccept was called
 		return false
 	} else if reportedUpkeep.Trigger.BlockNumber < v.checkBlockNumber {
+		// We already accepted a report for a higher checkBlockNumber, so don't try to transmit
 		return false
 	} else if reportedUpkeep.Trigger.BlockNumber == v.checkBlockNumber {
 		return true
 	} else {
-		c.logger.Printf("libocr should call shouldAccept before shouldTransmit")
+		// We never saw this report for such a high block number, so don't try to transmit
+		// Can happen in edge cases when plugin restarts after shouldAccept was called
 		return false
 	}
 }
@@ -94,26 +95,32 @@ func (c *coordinator) PreProcess(_ context.Context, payloads []ocr2keepers.Upkee
 func (c *coordinator) ShouldProcess(payload ocr2keepers.UpkeepPayload) bool {
 	if v, ok := c.cache.Get(payload.WorkID); ok {
 		if v.isTransmissionPending {
+			// This workID has a pending transmit, should not process it
 			return false
 		} else {
 			switch c.upkeepTypeGetter(payload.UpkeepID) {
 			case ocr2keepers.LogTrigger:
 				switch v.transmitType {
 				case ocr2keepers.PerformEvent:
+					// For log triggers, a particular workID should only ever be performed once
 					return false
 				default:
+					// There was an attempt to perform this workID, but it failed, so should be processed again
 					return true
 				}
 			case ocr2keepers.ConditionTrigger:
 				switch v.transmitType {
 				case ocr2keepers.PerformEvent:
+					// For conditionals, a particular workID should only be checked after its last perform
 					return payload.Trigger.BlockNumber > v.transmitBlockNumber
 				default:
+					// There was an attempt to check this workID, but it failed, so should be processed again
 					return true
 				}
 			}
 		}
 	}
+	// If we have never seen this workID before, then we should process it
 	return true
 }
 
@@ -131,15 +138,16 @@ func (c *coordinator) checkEvents(ctx context.Context) error {
 
 		if v, ok := c.cache.Get(event.WorkID); ok {
 			if event.CheckBlock < v.checkBlockNumber {
-				c.logger.Printf("Ignoring event in transaction %s from older report (block %v)", event.TransactionHash, event.CheckBlock)
+				c.logger.Printf("Ignoring event in transaction %s from older report (block %v) while waiting for (block %v)", event.TransactionHash, event.CheckBlock, v.checkBlockNumber)
 			} else if event.CheckBlock == v.checkBlockNumber {
 				c.cache.Set(event.WorkID, record{
 					checkBlockNumber:      v.checkBlockNumber,
-					isTransmissionPending: true,
+					isTransmissionPending: false,
 					transmitType:          event.Type,
 					transmitBlockNumber:   event.TransmitBlock,
 				}, util.DefaultCacheExpiration)
 			} else {
+				c.logger.Printf("Got event in transaction %s from newer report (block %v) while waiting for (block %v)", event.TransactionHash, event.CheckBlock, v.checkBlockNumber)
 				c.cache.Set(event.WorkID, record{
 					checkBlockNumber:      event.CheckBlock,
 					isTransmissionPending: false,
@@ -161,14 +169,14 @@ func (c *coordinator) run() {
 			startTime := time.Now()
 
 			if err := c.checkEvents(context.Background()); err != nil {
-				c.logger.Printf("failed to check events: %s", err)
+				c.logger.Printf("failed to check for transmit events: %s", err)
 			}
 
 			// attempt to adhere to a cadence of at least every second
 			// a slow DB will cause the cadence to increase. these cases are logged
 			diff := time.Since(startTime)
 			if diff > cadence {
-				c.logger.Printf("log poll took %dms to complete; expected cadence is %dms; check database indexes and other performance improvements", diff/time.Millisecond, cadence/time.Millisecond)
+				c.logger.Printf("check transmit events took %dms to complete; expected cadence is %dms; check database indexes and other performance improvements", diff/time.Millisecond, cadence/time.Millisecond)
 				// start again immediately
 				timer.Reset(time.Microsecond)
 			} else {
