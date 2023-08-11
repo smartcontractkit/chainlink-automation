3b3e45fb7fa72c7f7ca2bb16aba3ae8bc86c2f0f
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-08-11 22:36:25 +0100

    cleanup

--- pkg/v3/observation.go
+++ pkg/v3/observation.go
@@ -2,33 +2,9 @@ package ocr2keepers
 
 import (
 	"encoding/json"
-	"fmt"
 
 	ocr2keepers "github.com/smartcontractkit/ocr2keepers/pkg/v3/types"
 )
-
-type ObservationMetadataKey string
-
-const (
-	BlockHistoryObservationKey     ObservationMetadataKey = "blockHistory"
-	SampleProposalObservationKey   ObservationMetadataKey = "sampleProposals"
-	RecoveryProposalObservationKey ObservationMetadataKey = "recoveryProposals"
-)
-
-var (
-	ErrInvalidMetadataKey = fmt.Errorf("invalid metadata key")
-	ErrWrongDataType      = fmt.Errorf("wrong data type")
-	ErrBlockNotAvailable  = fmt.Errorf("coordinated block not available in outcome")
-)
-
-func ValidateObservationMetadataKey(key ObservationMetadataKey) error {
-	switch key {
-	case BlockHistoryObservationKey:
-		return nil
-	default:
-		return fmt.Errorf("%w: %s", ErrInvalidMetadataKey, key)
-	}
-}
 
 // AutomationObservation models the local automation view sent by a single node
 // to the network upon which they later get agreement
@@ -66,6 +42,8 @@ func ValidateAutomationObservation(o AutomationObservation) error {
 	// TODO: Only eligible results should be sent and those that have 0 error state
 	// TODO: WorkID should be validated
 	// TODO: Observations should not have duplicate results
+	// proposals should not have dplicate workIDs
+	// blockHistory should not have duplicate numbers
 	// TODO: Validate upkeepProposals and blockHistory
 
 	return nil
