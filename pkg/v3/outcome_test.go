0c9d1e7df111785dedf19fd02d5ff1db38f238ec
Akshay Aggarwal <akshay.aggarwal@smartcontract.com>
2023-08-14 12:27:33 +0100

    add outcome test

--- pkg/v3/outcome_test.go
+++ pkg/v3/outcome_test.go
@@ -1,1 +1,22 @@
 package ocr2keepers
+
+import (
+	"testing"
+
+	"github.com/smartcontractkit/ocr2keepers/pkg/v3/types"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestValidAutomationOutcome(t *testing.T) {
+	ao := AutomationOutcome{
+		AgreedPerformables: []types.CheckResult{validConditionalResult, validLogResult},
+		SurfacedProposals:  [][]types.CoordinatedBlockProposal{{validConditionalProposal, validLogProposal}},
+	}
+	encoded, err := ao.Encode()
+	assert.NoError(t, err, "no error in encoding valid automation outcome")
+
+	decoded, err := DecodeAutomationOutcome(encoded, mockUpkeepTypeGetter, mockWorkIDGenerator)
+	assert.NoError(t, err, "no error in decoding valid automation outcome")
+
+	assert.Equal(t, ao, decoded, "final result from encoding and decoding should match")
+}
