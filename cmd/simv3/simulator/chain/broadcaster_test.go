8f99402a453612825fb9a5b98d46c9faf60b6a8a
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2023-09-12 12:06:06 -0500

    remove panic recover test since it causes a race test failure

--- cmd/simv3/simulator/chain/broadcaster_test.go
+++ cmd/simv3/simulator/chain/broadcaster_test.go
@@ -42,36 +42,6 @@ func TestBlockBroadcaster(t *testing.T) {
 	assert.True(t, loader.called)
 }
 
-func TestBlockBroadcaster_Recover_On_Closed_Subscriber_Channel(t *testing.T) {
-	t.Parallel()
-
-	conf := config.Blocks{
-		Genesis:    big.NewInt(10),
-		Cadence:    config.Duration(50 * time.Millisecond),
-		Jitter:     config.Duration(10 * time.Millisecond),
-		Duration:   5,
-		EndPadding: 0,
-	}
-	maxDelay := 10
-	logger := log.New(io.Discard, "", 0)
-	loader := new(mockBlockLoader)
-	broadcaster := chain.NewBlockBroadcaster(conf, maxDelay, logger, loader.Load)
-
-	sub1ID, chBlocks1 := broadcaster.Subscribe(true)
-	sub2ID, chBlocks2 := broadcaster.Subscribe(true)
-	_ = broadcaster.Start()
-
-	close(chBlocks1)
-
-	<-chBlocks2
-
-	broadcaster.Unsubscribe(sub1ID)
-	broadcaster.Unsubscribe(sub2ID)
-	broadcaster.Stop()
-
-	assert.True(t, loader.called)
-}
-
 func TestBlockBroadcaster_Close_After_Limit(t *testing.T) {
 	t.Parallel()
 
