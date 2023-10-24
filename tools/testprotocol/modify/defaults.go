38b74bd616bf2b7003f23afd1d61ef9523e44cdd
Awbrey Hughlett <awbrey.hughlett@smartcontract.com>
2023-10-24 09:24:57 -0500

    add protocol modifier functions for testing purposes

---
+++ tools/testprotocol/modify/defaults.go
@@ -0,0 +1,107 @@
+package modify
+
+import "github.com/smartcontractkit/ocr2keepers/pkg/v3/types"
+
+var (
+	ObservationModifiers = []NamedModifier{
+		Modify(
+			"set all proposals to block 1",
+			AsObservation(
+				WithProposalBlockAs(types.BlockNumber(1)))),
+		Modify(
+			"set all proposals to block 1_000_000_000",
+			AsObservation(
+				WithProposalBlockAs(types.BlockNumber(1_000_000_000)))),
+		Modify(
+			"set all proposals to block 0",
+			AsObservation(
+				WithProposalBlockAs(types.BlockNumber(0)))),
+		Modify(
+			"set all performables to block 1",
+			AsObservation(
+				WithPerformableBlockAs(types.BlockNumber(1)))),
+		Modify(
+			"set all performables to block 1_000_000_000",
+			AsObservation(
+				WithPerformableBlockAs(types.BlockNumber(1_000_000_000)))),
+		Modify(
+			"set all performables to block 0",
+			AsObservation(
+				WithPerformableBlockAs(types.BlockNumber(0)))),
+		Modify(
+			"set all block history numbers to 0",
+			AsObservation(
+				WithBlockHistoryBlockAs(0))),
+		Modify(
+			"set all block history numbers to 1",
+			AsObservation(
+				WithBlockHistoryBlockAs(1))),
+		Modify(
+			"set all block history numbers to 1_000_000_000",
+			AsObservation(
+				WithBlockHistoryBlockAs(1_000_000_000))),
+	}
+
+	OutcomeModifiers = []NamedModifier{
+		Modify(
+			"set all proposals to block 1",
+			AsOutcome(
+				WithProposalBlockAs(types.BlockNumber(1)))),
+		Modify(
+			"set all proposals to block 1_000_000_000",
+			AsOutcome(
+				WithProposalBlockAs(types.BlockNumber(1_000_000_000)))),
+		Modify(
+			"set all proposals to block 0",
+			AsOutcome(
+				WithProposalBlockAs(types.BlockNumber(0)))),
+		Modify(
+			"set all performables to block 1",
+			AsOutcome(
+				WithPerformableBlockAs(types.BlockNumber(1)))),
+		Modify(
+			"set all performables to block 1_000_000_000",
+			AsOutcome(
+				WithPerformableBlockAs(types.BlockNumber(1_000_000_000)))),
+		Modify(
+			"set all performables to block 0",
+			AsOutcome(
+				WithPerformableBlockAs(types.BlockNumber(0)))),
+	}
+
+	ObservationInvalidValueModifiers = []NamedByteModifier{
+		ModifyBytes(
+			"set block value to empty string",
+			WithModifyKeyValue("BlockNumber", func(_ string, value interface{}) interface{} {
+				return ""
+			})),
+		ModifyBytes(
+			"set block value to negative number",
+			WithModifyKeyValue("BlockNumber", func(_ string, value interface{}) interface{} {
+				return -1
+			})),
+		ModifyBytes(
+			"set block value to very large number as string",
+			WithModifyKeyValue("BlockNumber", func(_ string, value interface{}) interface{} {
+				return "98989898989898989898989898989898989898989898"
+			})),
+	}
+
+	InvalidBlockModifiers = []NamedByteModifier{
+		ModifyBytes(
+			"set block value to empty string",
+			WithModifyKeyValue("BlockNumber", func(_ string, value interface{}) interface{} {
+				return ""
+			})),
+		ModifyBytes(
+			"set block value to negative number",
+			WithModifyKeyValue("BlockNumber", func(_ string, value interface{}) interface{} {
+				return -1
+			})),
+		ModifyBytes(
+			"set block value to very large number as string",
+			WithModifyKeyValue("BlockNumber", func(_ string, value interface{}) interface{} {
+				return "98989898989898989898989898989898989898989898"
+			})),
+	}
+)
---
+++ tools/testprotocol/modify/struct.go
@@ -0,0 +1,122 @@
+package modify
+
+import (
+	"context"
+	"fmt"
+
+	ocr2keepers "github.com/smartcontractkit/ocr2keepers/pkg/v3"
+	"github.com/smartcontractkit/ocr2keepers/pkg/v3/types"
+	ocr2keeperstypes "github.com/smartcontractkit/ocr2keepers/pkg/v3/types"
+)
+
+type NamedModifier func(context.Context, interface{}, error) (string, []byte, error)
+type ObservationModifier func(context.Context, ocr2keepers.AutomationObservation, error) ([]byte, error)
+type OutcomeModifier func(context.Context, ocr2keepers.AutomationOutcome, error) ([]byte, error)
+type ProposalModifier func(proposals []types.CoordinatedBlockProposal) []types.CoordinatedBlockProposal
+type PerformableModifier func(performables []types.CheckResult) []types.CheckResult
+type BlockHistoryModifier func(history ocr2keeperstypes.BlockHistory) ocr2keeperstypes.BlockHistory
+
+func WithProposalUpkeepIDAs(upkeepID types.UpkeepIdentifier) ProposalModifier {
+	return func(proposals []types.CoordinatedBlockProposal) []types.CoordinatedBlockProposal {
+		for _, proposal := range proposals {
+			proposal.UpkeepID = upkeepID
+		}
+
+		return proposals
+	}
+}
+
+func WithProposalBlockAs(block types.BlockNumber) ProposalModifier {
+	return func(proposals []types.CoordinatedBlockProposal) []types.CoordinatedBlockProposal {
+		for _, proposal := range proposals {
+			proposal.Trigger.BlockNumber = block
+		}
+
+		return proposals
+	}
+}
+
+func WithPerformableBlockAs(block types.BlockNumber) PerformableModifier {
+	return func(performables []types.CheckResult) []types.CheckResult {
+		for _, performable := range performables {
+			performable.Trigger.BlockNumber = block
+		}
+
+		return performables
+	}
+}
+
+func WithBlockHistoryBlockAs(number uint64) BlockHistoryModifier {
+	return func(history ocr2keeperstypes.BlockHistory) ocr2keeperstypes.BlockHistory {
+		for idx := range history {
+			history[idx].Number = ocr2keeperstypes.BlockNumber(number)
+		}
+
+		return history
+	}
+}
+
+func AsObservation(modifiers ...interface{}) ObservationModifier {
+	return func(ctx context.Context, observation ocr2keepers.AutomationObservation, err error) ([]byte, error) {
+		for _, IModifier := range modifiers {
+			switch modifier := IModifier.(type) {
+			case ProposalModifier:
+				observation.UpkeepProposals = modifier(observation.UpkeepProposals)
+			case PerformableModifier:
+				observation.Performable = modifier(observation.Performable)
+			case BlockHistoryModifier:
+				observation.BlockHistory = modifier(observation.BlockHistory)
+			default:
+				return nil, fmt.Errorf("unexpected modifier type")
+			}
+		}
+
+		return observation.Encode()
+	}
+}
+
+func AsOutcome(modifiers ...interface{}) OutcomeModifier {
+	return func(ctx context.Context, outcome ocr2keepers.AutomationOutcome, err error) ([]byte, error) {
+		for _, IModifier := range modifiers {
+			switch modifier := IModifier.(type) {
+			case ProposalModifier:
+				for idx, proposals := range outcome.SurfacedProposals {
+					outcome.SurfacedProposals[idx] = modifier(proposals)
+				}
+			case PerformableModifier:
+				outcome.AgreedPerformables = modifier(outcome.AgreedPerformables)
+			default:
+				return nil, fmt.Errorf("unexpected modifier type")
+			}
+		}
+
+		return outcome.Encode()
+	}
+}
+
+func Modify(name string, iModifier interface{}) NamedModifier {
+	return func(ctx context.Context, value interface{}, err error) (string, []byte, error) {
+		switch modifier := iModifier.(type) {
+		case ObservationModifier:
+			observation, ok := value.(ocr2keepers.AutomationObservation)
+			if !ok {
+				return name, nil, fmt.Errorf("value not an observation")
+			}
+
+			bts, err := modifier(ctx, observation, err)
+
+			return name, bts, err
+		case OutcomeModifier:
+			outcome, ok := value.(ocr2keepers.AutomationOutcome)
+			if !ok {
+				return name, nil, fmt.Errorf("value not an outcome")
+			}
+
+			bts, err := modifier(ctx, outcome, err)
+
+			return name, bts, err
+		default:
+			return name, nil, fmt.Errorf("unrecognized modifier type")
+		}
+	}
+}
