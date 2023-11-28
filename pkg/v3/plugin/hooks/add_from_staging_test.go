45c8555195220564765096cabd318b3e13434cca
amirylm <amirylm.dev@gmail.com>
2023-11-28 13:26:00 -0300

    added limits tests

--- pkg/v3/plugin/hooks/add_from_staging_test.go
+++ pkg/v3/plugin/hooks/add_from_staging_test.go
@@ -2,6 +2,7 @@ package hooks
 
 import (
 	"bytes"
+	"fmt"
 	"log"
 	"testing"
 
@@ -183,4 +184,66 @@ func TestAddFromStagingHook_RunHook(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestAddFromStagingHook_RunHook_Limits(t *testing.T) {
+	tests := []struct {
+		name     string
+		n        int
+		limit    int
+		expected int
+	}{
+		{
+			name:     "limit is less than results",
+			n:        1000,
+			limit:    100,
+			expected: 100,
+		},
+		{
+			name:     "limit is greater than results",
+			n:        100,
+			limit:    200,
+			expected: 100,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			mockResultStore, mockCoordinator := getMocks(tt.n)
+			var logBuf bytes.Buffer
+			logger := log.New(&logBuf, "", 0)
+			addFromStagingHook := NewAddFromStagingHook(mockResultStore, mockCoordinator, logger)
+
+			rSrc := [16]byte{1, 1, 2, 2, 3, 3, 4, 4}
+			obs := &ocr2keepersv3.AutomationObservation{}
+
+			err := addFromStagingHook.RunHook(obs, tt.limit, rSrc)
+			assert.NoError(t, err)
+			assert.Len(t, obs.Performable, tt.expected)
+
+			// Run the hook again with the same random source
+			// and assert that the results are the same
+			mockResultStore2, mockCoordinator2 := getMocks(tt.n)
+			addFromStagingHook2 := NewAddFromStagingHook(mockResultStore2, mockCoordinator2, logger)
+
+			obs2 := &ocr2keepersv3.AutomationObservation{}
+			err2 := addFromStagingHook2.RunHook(obs2, tt.limit, rSrc)
+			assert.NoError(t, err2)
+			assert.Len(t, obs.Performable, tt.expected)
+			assert.Equal(t, obs.Performable, obs2.Performable)
+		})
+	}
+}
+
+func getMocks(n int) (*mocks.MockResultStore, *mocks.MockCoordinator) {
+	mockResults := make([]types.CheckResult, n)
+	for i := 0; i < n; i++ {
+		mockResults[i] = types.CheckResult{UpkeepID: [32]byte{uint8(i)}, WorkID: fmt.Sprintf("10%d", i)}
+	}
+	mockResultStore := &mocks.MockResultStore{}
+	mockResultStore.On("View").Return(mockResults, nil)
+	mockCoordinator := &mocks.MockCoordinator{}
+	mockCoordinator.On("FilterResults", mock.Anything).Return(mockResults, nil)
+
+	return mockResultStore, mockCoordinator
 }
