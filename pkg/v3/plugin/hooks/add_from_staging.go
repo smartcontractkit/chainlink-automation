fdf351d5a88d0d806e451a6de578fb3ea4182ca3
amirylm <amirylm.dev@gmail.com>
2024-04-05 16:05:45 +0300

    Option 4: change random seed every x round

--- pkg/v3/plugin/hooks/add_from_staging.go
+++ pkg/v3/plugin/hooks/add_from_staging.go
@@ -1,9 +1,13 @@
 package hooks
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"sort"
+	"sync"
+
+	"github.com/smartcontractkit/chainlink-common/pkg/types/automation"
 
 	ocr2keepersv3 "github.com/smartcontractkit/chainlink-automation/pkg/v3"
 	"github.com/smartcontractkit/chainlink-automation/pkg/v3/random"
@@ -11,18 +15,28 @@ import (
 	"github.com/smartcontractkit/chainlink-automation/pkg/v3/types"
 )
 
-func NewAddFromStagingHook(store types.ResultStore, coord types.Coordinator, logger *log.Logger) AddFromStagingHook {
-	return AddFromStagingHook{
-		store:  store,
-		coord:  coord,
-		logger: log.New(logger.Writer(), fmt.Sprintf("[%s | build hook:add-from-staging]", telemetry.ServiceName), telemetry.LogPkgStdFlags),
-	}
+type stagingHookState struct {
+	lastRandSrc [16]byte
+	shuffledIDs map[string]string
 }
 
 type AddFromStagingHook struct {
 	store  types.ResultStore
 	logger *log.Logger
 	coord  types.Coordinator
+	state  stagingHookState
+	lock   sync.Mutex
+}
+
+func NewAddFromStagingHook(store types.ResultStore, coord types.Coordinator, logger *log.Logger) AddFromStagingHook {
+	return AddFromStagingHook{
+		store:  store,
+		coord:  coord,
+		logger: log.New(logger.Writer(), fmt.Sprintf("[%s | build hook:add-from-staging]", telemetry.ServiceName), telemetry.LogPkgStdFlags),
+		state: stagingHookState{
+			shuffledIDs: make(map[string]string),
+		},
+	}
 }
 
 // RunHook adds results from the store to the observation.
@@ -39,30 +53,43 @@ func (hook *AddFromStagingHook) RunHook(obs *ocr2keepersv3.AutomationObservation
 	if err != nil {
 		return err
 	}
+
+	results = hook.orderResults(results, rSrc)
 	n := len(results)
-	maxResults := limit * 10
-	if len(results) > maxResults {
-		hook.logger.Printf("too many results in staging (%d) ignoring %d results", n, len(results)-maxResults)
-		// first we sort by workID, to ensure that the same workIDs are selected across nodes
-		sort.Slice(results, func(i, j int) bool {
-			return results[i].WorkID < results[j].WorkID
-		})
-		results = results[:maxResults]
-	}
-	// creating a map to hold the shuffled workIDs
-	shuffledIDs := make(map[string]string, len(results))
-	for _, result := range results {
-		shuffledIDs[result.WorkID] = random.ShuffleString(result.WorkID, rSrc)
-	}
-	// sort by the shuffled workID
-	sort.Slice(results, func(i, j int) bool {
-		return shuffledIDs[results[i].WorkID] < shuffledIDs[results[j].WorkID]
-	})
-	if len(results) > limit {
+	if n > limit {
 		results = results[:limit]
 	}
 	hook.logger.Printf("adding %d results to observation, out of %d available results", len(results), n)
 	obs.Performable = append(obs.Performable, results...)
 
 	return nil
+}
+
+func (hook *AddFromStagingHook) orderResults(results []automation.CheckResult, rSrc [16]byte) []automation.CheckResult {
+	hook.lock.Lock()
+	defer hook.lock.Unlock()
+
+	shuffledIDs := hook.updateShuffledIDs(results, rSrc)
+	// sort by the shuffled workID
+	sort.Slice(results, func(i, j int) bool {
+		return shuffledIDs[results[i].WorkID] < shuffledIDs[results[j].WorkID]
+	})
+
+	return results
+}
+
+func (hook *AddFromStagingHook) updateShuffledIDs(results []automation.CheckResult, rSrc [16]byte) map[string]string {
+	// once the random source changes, the workIDs needs to be shuffled again with the new source
+	if !bytes.Equal(hook.state.lastRandSrc[:], rSrc[:]) {
+		hook.state.lastRandSrc = rSrc
+		hook.state.shuffledIDs = make(map[string]string)
+	}
+
+	for _, result := range results {
+		if _, ok := hook.state.shuffledIDs[result.WorkID]; !ok {
+			hook.state.shuffledIDs[result.WorkID] = random.ShuffleString(result.WorkID, rSrc)
+		}
+	}
+
+	return hook.state.shuffledIDs
 }
