e447c8f0b37d81f3e0319536d67ed2ceffef290a
FelixFan1992 <fankejin@gmail.com>
2024-05-24 12:25:19 -0400

    upkeep

--- pkg/v3/flows/maliciousupkeep.go
+++ pkg/v3/flows/maliciousupkeep.go
@@ -2,6 +2,7 @@ package flows
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 	"math/big"
@@ -10,43 +11,31 @@ import (
 	common "github.com/smartcontractkit/chainlink-common/pkg/types/automation"
 
 	ocr2keepersv3 "github.com/smartcontractkit/chainlink-automation/pkg/v3"
-	"github.com/smartcontractkit/chainlink-automation/pkg/v3/postprocessors"
 	"github.com/smartcontractkit/chainlink-automation/pkg/v3/service"
 	"github.com/smartcontractkit/chainlink-automation/pkg/v3/telemetry"
 	"github.com/smartcontractkit/chainlink-automation/pkg/v3/tickers"
 	"github.com/smartcontractkit/chainlink-automation/pkg/v3/types"
 )
 
-// log trigger flow is the happy path entry point for log triggered upkeeps
+// newMaliciousUpkeepFlow is the happy path entry point for log triggered upkeeps
 func newMaliciousUpkeepFlow(
-	preprocessors []ocr2keepersv3.PreProcessor[*big.Int],
-	rs types.ResultStore,
-	rn ocr2keepersv3.Runner,
+	ms types.MetadataStore,
 	mup common.MaliciousUpkeepProvider,
 	maliciousUpkeepReportingInterval time.Duration,
 	logger *log.Logger,
 ) service.Recoverable {
-	post := postprocessors.NewCombinedPostprocessor(
-		postprocessors.NewEligiblePostProcessor(rs, telemetry.WrapLogger(logger, "log-trigger-eligible-postprocessor")),
-	)
-
-	obs := ocr2keepersv3.NewRunnableObserver(
-		preprocessors,
-		post,
-		rn,
-		ObservationProcessLimit,
-		log.New(logger.Writer(), fmt.Sprintf("[%s | log-trigger-observer]", telemetry.ServiceName), telemetry.LogPkgStdFlags),
-	)
+	obs := ocr2keepersv3.NewUpkeepIdsObserver()
 
 	timeTick := tickers.NewTimeTicker[[]*big.Int](maliciousUpkeepReportingInterval, obs, func(ctx context.Context, _ time.Time) (tickers.Tick[[]*big.Int], error) {
-		return maliciousUpkeepTick{logger: logger, mup: mup}, nil
-	}, log.New(logger.Writer(), fmt.Sprintf("[%s | log-trigger-ticker]", telemetry.ServiceName), telemetry.LogPkgStdFlags))
+		return maliciousUpkeepTick{logger: logger, ms: ms, mup: mup}, nil
+	}, log.New(logger.Writer(), fmt.Sprintf("[%s | malicious-upkeep-ticker]", telemetry.ServiceName), telemetry.LogPkgStdFlags))
 
 	return timeTick
 }
 
 type maliciousUpkeepTick struct {
 	mup    common.MaliciousUpkeepProvider
+	ms     types.MetadataStore
 	logger *log.Logger
 }
 
@@ -55,9 +44,15 @@ func (et maliciousUpkeepTick) Value(ctx context.Context) ([]*big.Int, error) {
 		return nil, nil
 	}
 
-	logs, err := et.mup.GetMaliciousUpkeepIds(ctx)
+	ids, err := et.mup.GetMaliciousUpkeepIds(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-	et.logger.Printf("%d logs returned by log provider", len(logs))
+	// save it to metadata_store
+	et.ms.SetMaliciousUpkeeps(ids)
 
-	return logs, err
+	et.logger.Printf("%d malicious upkeeps returned by malicious upkeep provider. they are %s", len(ids), ids)
+
+	return nil, errors.New("no-op")
 }
